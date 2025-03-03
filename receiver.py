001ecc5e66e5b89a1c118a675a61ece2b8adeb45
frc-dock <olinhairlab@gmail.com>
2025-03-03 18:37:28 -0500

    update receiver for 2 way connection

--- receiver.py
+++ receiver.py
@@ -1,21 +1,29 @@
 from pymavlink import mavutil
 import time
 
-# Connect rasp pi to mavlink stream
-# master = 'udp:192.168.34.71:14551' 
-master_ip = 'udp:0.0.0.0:14551' # open to all connections at port 14550
-master = mavutil.mavlink_connection(master_ip)
+# IP addresses
+RECEIVER_IP = 'udp:0.0.0.0:14551'  # Receiving messages at port 14551
+COMPANION_IP = 'udpout:192.168.34.71:14552'  # Sending messages to companion PC
 
-print(f"Connected to MAVLink stream at {master_ip}")
+# Establish connections
+receiver = mavutil.mavlink_connection(RECEIVER_IP)  # Receives messages
+sender = mavutil.mavlink_connection(COMPANION_IP)  # Sends messages
+
+print(f"Connected to MAVLink stream (receiver) at {RECEIVER_IP}")
+print(f"Connected to companion PC (sender) at {COMPANION_IP}")
 print("Waiting for messages...")
 
 while True:
-    # Receive a message
-    msg = master.recv_match(blocking=True)
-    
+    # Listne for incoming `STATUSTEXT` type message
+    msg = receiver.recv_match(type='STATUSTEXT', blocking=True)
+
     if msg:
-        # Print the received message
+        # Extract received text
         received_data = msg.text
         print(f"Received mock data: {received_data}")
+
+        # Send a response to the companion PC
+        outgoing_data = "Echo from RaspPi"
+        sender.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, outgoing_data.encode('ascii'))
 
     time.sleep(1)
