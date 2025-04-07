96a22c96c5af78e62288a4d018a4da09c8342099
frc-dock <olinhairlab@gmail.com>
2025-04-07 19:08:50 -0400

    add code for posting to aws

---
+++ dock/receive_and_post.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import datetime
+import socket
+
+def send_sensor_data(payload)->None:
+    SERVER_URL = "http://3.131.219.242:8000/api/data"
+    try:
+        # POST the payload as JSON
+        response = requests.post(SERVER_URL, json=payload)
+        
+        # Check status code and print server response
+        if response.status_code == 201:
+            print("Data sent successfully!")
+            print("Server response:", response.json())
+        else:
+            print("Failed to send data. Status code:", response.status_code)
+            print("Server response:", response.text)
+    except Exception as e:
+        print("Error sending sensor data:", e)
+
+
+def main()->None:
+    HOST = '192.168.4.1'
+    PORT = 5000
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Server listening on {HOST}:{PORT}...")
+        while True:
+            conn, addr = s.accept() # waits for connection
+            with conn:
+                print(f"Incoming data from {addr[0]}")
+                data = b""
+                while True:
+                    packet = conn.recv(4096)
+                    if not packet:
+                        break
+                    data += packet
+
+                # Decode JSON to python list/dict
+                json_data = json.loads(data.decode('utf-8'))
+                print("data received!")
+
+                ## Save to file
+                # with open('received_data.json', 'w') as f:
+                #     json.dump(json_data, f, indent=2)
+
+if __name__ == "__main__":
+    main()
