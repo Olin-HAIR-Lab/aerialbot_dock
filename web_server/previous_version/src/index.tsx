042ac9b28394638fe23851512bd80e8de127edb3
redhotchili0909 <cpark1@olin.edu>
2026-04-24 09:12:02 -0400

    Remove excess files

--- web_server/previous_version/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-const container = document.getElementById("root");
-// TypeScript null check for the container
-if (!container) throw new Error("Root element not found");
-const root = createRoot(container);
-root.render(<App />);
